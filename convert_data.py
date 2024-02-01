de43cc93f7e8d4adf6bcb47d38b408e01ef8c43b
Anas Gouda <anas.gouda@tu-dortmund.de>
2024-02-01 17:21:37 +0100

    generate mask and mask_vis

--- convert_data.py
+++ convert_data.py
@@ -60,8 +60,12 @@ for scene_num in range(10):
         with open(old_json_file, 'r') as f:
             json_data = json.load(f)
         # read camera extrinsics
-        camera_extrinsics = json_data["camera"]["extrinsics"]
-        camera_extrinsics = np.array(camera_extrinsics).reshape(4,4)
+        camera_extrinsics_w2c = json_data["camera"]["extrinsics"]
+        camera_extrinsics_w2c = np.array(camera_extrinsics_w2c).reshape(4, 4)
+        # calculate the inverse H_c2w
+        H_c2w = np.eye(4)
+        H_c2w[:3, :3] = camera_extrinsics_w2c[:3, :3].T
+        H_c2w[:3, 3] = -camera_extrinsics_w2c[:3, :3].T @ camera_extrinsics_w2c[:3, 3]
         # read camera intrinsics
         camera_intrinsics = json_data["camera"]["intrinsics"]
         camera_intrinsics = np.array(camera_intrinsics).reshape(3,3)
@@ -69,8 +73,8 @@ for scene_num in range(10):
         frame_cam = {}
         frame_cam["cam_k"] = camera_intrinsics.flatten().tolist()
         frame_cam["depth_scale"] = 1.0
-        translation = camera_extrinsics[:3,3]
-        rotation = camera_extrinsics[:3,:3]
+        translation = camera_extrinsics_w2c[:3, 3]
+        rotation = camera_extrinsics_w2c[:3, :3]
         frame_cam["cam_t_w2c"] = translation.tolist()
         frame_cam["cam_R_w2c"] = rotation.flatten().tolist()
         scene_camera[frame_num] = frame_cam
@@ -106,14 +110,14 @@ for scene_num in range(10):
         camera_intrinsics_o3d = o3d.camera.PinholeCameraIntrinsic(width, height, camera_intrinsics[0,0], camera_intrinsics[1,1], camera_intrinsics[0,2], camera_intrinsics[1,2])
         frame_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image_o3d, camera_intrinsics_o3d)
         # transform point cloud to the world coordinate frame
-        frame_pcd.transform(np.linalg.inv(camera_extrinsics))
+        frame_pcd.transform(H_c2w)
         # make origin coordinate frame at the world frame
         origin_mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100, origin=[0, 0, 0])
         # make another coordinate frame at the camera frame
         camera_mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100, origin=[0, 0, 0])
-        camera_mesh_frame.transform(np.linalg.inv(camera_extrinsics))
+        camera_mesh_frame.transform(H_c2w)
         # visualize point cloud
-        o3d.visualization.draw_geometries([frame_pcd, origin_mesh_frame, camera_mesh_frame])
+        #o3d.visualization.draw_geometries([frame_pcd, origin_mesh_frame, camera_mesh_frame])
         # loop through each object in frame_gt, load its model, transform the object then visualize all objects
         objects_visualize = []
         for obj in frame_gt:
@@ -125,18 +129,17 @@ for scene_num in range(10):
             H_m2c[:3,3] = np.array(obj["cam_t_m2c"])
             H_m2c[:3,:3] = np.array(obj["cam_R_m2c"])
             # transform object to the world coordinate frame
-            H_w2c = camera_extrinsics
-            H_c2w = np.linalg.inv(H_w2c)
+            H_w2c = camera_extrinsics_w2c
             H_m2w = H_c2w @ H_m2c
 
             obj_pcd.transform(H_m2w)
             objects_visualize.append(obj_pcd)
-        o3d.visualization.draw_geometries(objects_visualize + [frame_pcd, origin_mesh_frame, camera_mesh_frame])
+        #o3d.visualization.draw_geometries(objects_visualize + [frame_pcd, origin_mesh_frame, camera_mesh_frame])
         # ==============================================================================================================
 
         # make open3d Scene, to help generating depth images and masks
         vis = o3d.visualization.Visualizer()
-        vis.create_window(width=width, height=height, left=50, top=50, visible=True)
+        vis.create_window(width=width, height=height, top=0, left=0, visible=True)
         ctr = vis.get_view_control()
 
         # add any of the objects just for testing
@@ -145,83 +148,89 @@ for scene_num in range(10):
         #vis.update_renderer()
         #vis.remove_geometry(frame_pcd)
 
-        # make visualizer look at z where up is -y
-        ctr.set_lookat([0, 0, 1])
-        ctr.set_up([0, -1, 0])
-
         pinhole_camera_parameters = o3d.camera.PinholeCameraParameters()
         camera_intrinsics_o3d = o3d.camera.PinholeCameraIntrinsic(width, height, camera_intrinsics[0,0], camera_intrinsics[1,1], camera_intrinsics[0,2], camera_intrinsics[1,2])
         pinhole_camera_parameters.intrinsic = camera_intrinsics_o3d
-        pinhole_camera_parameters.extrinsic = np.linalg.inv(camera_extrinsics)
+        pinhole_camera_parameters.extrinsic = camera_extrinsics_w2c
+
+        # set the camera parameters will be done after adding the first object to the scene with reset_bounding_box=True
+        # somehow if the camera parameters are set before adding the first object to the scene the visualizer doesn't work
+        # this could be a bug in open3d
 
         # set point size in the visualizer to 1
         opt = vis.get_render_option()
         opt.point_size = 3
         opt.show_coordinate_frame = True
 
-        # generate mask for each object in frame_gt
-        frame_masks = []
-        for anno_id in range(len(frame_gt)):
-            # load object mesh and transform it to the world coordinate frame
-            obj_id = frame_gt[anno_id]["obj_id"]
+        # load all objects meshes and transform them to the world coordinate frame
+        objects_meshes = []
+        for obj in frame_gt:
+            obj_id = obj["obj_id"]
+            # load object mesh
             obj_mesh = o3d.io.read_triangle_mesh(os.path.join(bop_models_path, f'obj_{obj_id:06}.ply'))
+            # transform object mesh
             H_m2c = np.eye(4)
-            H_m2c[:3,3] = np.array(frame_gt[anno_id]["cam_t_m2c"])
-            H_m2c[:3,:3] = np.array(frame_gt[anno_id]["cam_R_m2c"])
-
+            H_m2c[:3,3] = np.array(obj["cam_t_m2c"])
+            H_m2c[:3,:3] = np.array(obj["cam_R_m2c"])
             # transform object to the world coordinate frame
-            H_w2c = camera_extrinsics
-            H_c2w = np.linalg.inv(H_w2c)
+            H_w2c = camera_extrinsics_w2c
             H_m2w = H_c2w @ H_m2c
-
             obj_mesh.transform(H_m2w)
-            vis.add_geometry(obj_mesh, reset_bounding_box=True)
-            # render the scene
+            objects_meshes.append(obj_mesh)
+
+        # make a fake depth image with all objects in it
+        # add all objects to the scene
+        for obj in objects_meshes:
+            vis.add_geometry(obj, reset_bounding_box=True)
+        # set the camera parameters
+        ctr.convert_from_pinhole_camera_parameters(pinhole_camera_parameters, allow_arbitrary=True)
+        # render the scene
+        vis.poll_events()
+        vis.update_renderer()
+        # capture depth image
+        fake_all_objs_depth_image_o3d = vis.capture_depth_float_buffer(do_render=True)
+        # convert depth image to numpy array
+        fake_all_objs_depth_image = np.asarray(fake_all_objs_depth_image_o3d)
+        # remove all objects from the scene
+        vis.clear_geometries()
+
+        # generate mask and mask_vis for each object in frame_gt
+        frame_masks = []
+        frame_masks_vis = []
+        for anno_id in range(len(frame_gt)):
+            # add object to scene
+            obj_id = frame_gt[anno_id]["obj_id"]
+            obj_mesh = objects_meshes[anno_id]
+            vis.add_geometry(obj_mesh, reset_bounding_box=False)
             vis.poll_events()
             vis.update_renderer()
-
-            #o3d.visualization.draw_geometries([obj_mesh, frame_pcd ,origin_mesh_frame, camera_mesh_frame])
-
-            # this should be outside the loop
-            ctr.convert_from_pinhole_camera_parameters(pinhole_camera_parameters, allow_arbitrary=True)
-            vis.poll_events()
-            vis.update_renderer()
-
 
             # capture depth image
-            fake_depth_image_o3d = vis.capture_depth_float_buffer(do_render=False)
+            fake_obj_depth_image_o3d = vis.capture_depth_float_buffer(do_render=True)
             # convert depth image to numpy array
-            fake_depth_image = np.asarray(fake_depth_image_o3d)
+            fake_obj_depth_image = np.asarray(fake_obj_depth_image_o3d)
             # generate mask
             mask = np.zeros((height, width))
-            mask[fake_depth_image > 0] = 255
+            mask[fake_obj_depth_image > 0] = 255
             # save mask
             mask_file = os.path.join(bop_scene_path, 'mask', f'{frame_num:06}_{anno_id:06}.png')
             cv2.imwrite(mask_file, mask)
             # append mask to frame_masks
             frame_masks.append(mask)
-            # remove object from scene
-            vis.clear_geometries()
 
-        # generate mask_vis for each object in frame_gt
-        frame_masks_vis = []
-        # add all objects to scene to generate a fake depth image
-        for obj in objects_visualize:
-            vis.add_geometry(obj, reset_bounding_box=False)
-        # capture the fake depth image
-        fake_depth_image_o3d = vis.capture_depth_float_buffer(do_render=True)
-        # convert depth image to numpy array
-        fake_depth_image = np.asarray(fake_depth_image_o3d)
-        # loop through each object in frame_gt
-        for anno_id, mask in enumerate(frame_masks):
             # generate mask_vis
-            mask_vis = np.zeros((height, width))
-            # mask_vis = intersection between mask and fake_depth_image
-            mask_vis[np.logical_and(mask, fake_depth_image > 0)] = 255
+            # the mask are the equal pixels values between the fake_all_objs_depth_image and the fake_obj_depth_image
+            # mask the image after that with the mask (the visible mask) to remove the background
+            equal_mask = np.equal(fake_all_objs_depth_image, fake_obj_depth_image)
+            mask_vis = equal_mask.astype(np.uint8) * mask
             # save mask_vis
             mask_vis_file = os.path.join(bop_scene_path, 'mask_vis', f'{frame_num:06}_{anno_id:06}.png')
             cv2.imwrite(mask_vis_file, mask_vis)
             # append mask_vis to frame_masks_vis
+            frame_masks_vis.append(mask_vis)
+
+            # remove object from scene
+            vis.clear_geometries()
 
         # make scene_gt_info
         # all bboxes are (x, y, width, height)
